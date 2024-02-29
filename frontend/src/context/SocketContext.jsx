cd58efff07b2c3fb627c35778e94bbeb1337d6c8
morningstar005 <hemantbhatnagar0005@gmail.com>
2024-03-01 02:15:08 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
        if(authUser){
-        const socket = io("http://localhost:8500",{
+        const socket = io("https://chat-app-production-kuy9.onrender.com",{
             query:{
                 userId:authUser._id,
             }
